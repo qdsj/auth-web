a7c58b22530f17aaf221d743ed11326eef354753
zenos <zenos.chen@foxmail.com>
2025-01-12 19:48:00 +0800

    add router and tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import { Button } from "antd";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 function App() {
 	return (
 		<>
-			<Button>按钮</Button>
+			<BrowserRouter>
+				<Routes>
+					<Route path='/login' element={<Login />} />
+					<Route path='/register' element={<Register />} />
+					<Route path='/' element={<div>Auth Web</div>} />
+				</Routes>
+			</BrowserRouter>
 		</>
 	);
 }
---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,3 @@
+export default function Login() {
+	return <div className='bg-black text-white'>Login</div>;
+}
